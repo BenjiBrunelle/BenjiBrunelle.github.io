e5e9e113e5fec4006d0872f86ea5e255eb780f88
BenjiBrunelle <116543860+BenjiBrunelle@users.noreply.github.com>
2023-01-15 14:19:59 +0100

    Delete gangekalkulator directory

--- gangekalkulator/js/gange.js
+++
@@ -1,5 +0,0 @@
-
-var nummer1 = prompt("Skriv inn det første tallet:");
-var nummer2 = prompt("Skriv inn det andre tallet:");
-var resultat = nummer1 * nummer2;
-alert("Resultatet er: " + resultat);
