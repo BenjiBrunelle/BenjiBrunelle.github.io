a466711ec2b90e0d1836e648868855d8ae297b14
BenjiBrunelle <116543860+BenjiBrunelle@users.noreply.github.com>
2023-04-19 10:41:41 +0200

    Add files via upload

---
+++ prototype/Prototype-utvikling/javascript/prototype.js
@@ -0,0 +1,10 @@
+
+function toggleOptions() {
+    var options = document.getElementById("options");
+    if (options.style.display === "none") {
+      options.style.display = "block";
+    } else {
+      options.style.display = "none";
+    }
+}
+
